f3c2674c34653fa80cd1364e0397110d960616d2
Robert Heath <robert.b.heathii@gmail.com>
2020-08-26 11:33:28 -0500

    Test and code add_room

--- lib/house.rb
+++ lib/house.rb
@@ -1,3 +1,5 @@
+require './lib/room'
+
 class House
 
 attr_reader :price, :address, :rooms
@@ -6,5 +8,9 @@ attr_reader :price, :address, :rooms
     @price = price[1..-1].to_i
     @address = address
     @rooms = rooms
+  end
+
+  def add_room(room)
+    rooms << room
   end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -11,11 +11,20 @@ class RoomTest < Minitest::Test
 
   def test_house_has_attributes
     house = House.new("$400000","123 sugar lane")
-
     assert_equal 400000, house.price
     assert_equal "123 sugar lane", house.address
     assert_equal [], house.rooms
   end
 
-  
+  def test_add_room
+    house = House.new("$400000","123 sugar lande")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    assert_equal [room_1], house.rooms
+    house.add_room(room_2)
+    assert_equal [room_1, room_2], house.rooms
+  end
+
+
 end
